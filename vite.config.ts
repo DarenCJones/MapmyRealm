7df96197113b6332f3cc6693b8eee7531b9bdebd
Caitlin Jones <jones.caitlin.cs@gmail.com>
2025-03-26 22:50:28 -0400

    Alias components and routes folders

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/MapmyRealm/",
+  resolve: {
+    alias: {
+      routes: resolve(__dirname, "./src/routes"),
+      components: resolve(__dirname, "./src/components"),
+    },
+  },
 });
