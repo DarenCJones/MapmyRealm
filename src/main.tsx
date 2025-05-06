f690d1d4d1df0ae18f1dc3820bce96b2c24a8e6b
Caitlin Jones <jones.caitlin.cs@gmail.com>
2025-05-06 11:31:13 -0400

    test main

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <div className="page">
         <Routes />
+        <div>this main page is loading. Is routes?</div>
       </div>
       <Sidebar />
       <Footer />
