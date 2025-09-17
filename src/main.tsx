64378694baa896012a06e3fe84874a656d2a9c94
CaitlinJones <jones.caitlin.cs@gmail.com>
2025-09-16 20:43:14 -0400

    Update packages to use radix

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,30 @@ import Routes from "./Routes";
 import Sidebar from "components/Sidebar";
 import Footer from "components/Footer";
 import { BrowserRouter } from "react-router";
+import { Theme } from '@radix-ui/themes';
+import { ThemeProvider } from 'next-themes';
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <div className="page">
-        <Routes />
-      </div>
-      <Sidebar />
-      <Footer />
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='light'
+        enableSystem
+      >
+        <Theme
+          panelBackground='solid'
+          grayColor='sand'
+          accentColor='red'
+        >
+          <div className="page">
+            <Routes />
+          </div>
+          <Sidebar />
+          <Footer />
+        </Theme>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
