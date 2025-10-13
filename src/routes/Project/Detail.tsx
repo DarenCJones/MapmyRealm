666c7d35676c2734cb845115e912a8b900d00e3e
Daren Jones <DarenCjones@gmail.com>
2025-10-13 11:52:56 -0400

    fixed leaflet maps

--- src/routes/Project/Detail.tsx
+++ src/routes/Project/Detail.tsx
@@ -1,17 +1,39 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
+
 export default function Detail(){
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+
+
+<div>this is new map</div>
+
+      <MapContainer
+        className='h-[400px]'
+        center={[39.74739, -105]} // Denver area example
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+
+<div>this is new map</div>
+
+      <MapContainer className='h-[400px]' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+     
+    </>
   );
 }
